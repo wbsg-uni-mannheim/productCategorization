c887007535388c7f5d3fea28167cc3190e66daf3
abrinkmann <Alex0815@users.noreply.github.com>
2020-10-27 10:39:20 +0100

    Normalize data types

--- src/data/icecat/unify.py
+++ src/data/icecat/unify.py
@@ -10,4 +10,10 @@ def reduce_schema(df_original_dataset):
     df_dataset.rename(columns={'Title': 'title', 'Category.Name.Value': 'category', 'Brand': 'brand',
                                'pathlist_names': 'path_list'}, inplace=True)
 
+    # Convert dtype to string
+    df_dataset['title'] = df_dataset['title'].astype(str)
+    df_dataset['category'] = df_dataset['category'].astype(str)
+    df_dataset['description'] = df_dataset['description'].astype(str)
+    df_dataset['brand'] = df_dataset['brand'].astype(str)
+
     return df_dataset[['title', 'description', 'brand', 'category', 'path_list']]
--- src/data/rakuten/unify.py
+++ src/data/rakuten/unify.py
@@ -6,4 +6,8 @@ def reduce_schema(df_original_dataset):
 
     df_dataset.rename(columns={'CategoryID': 'category', 'pathlist': 'path_list'}, inplace=True)
 
+    # Convert dtype to string
+    df_dataset['title'] = df_dataset['title'].astype(str)
+    df_dataset['category'] = df_dataset['category'].astype(str)
+
     return df_dataset[['title', 'category', 'path_list']]
