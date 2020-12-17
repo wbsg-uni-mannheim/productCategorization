08f89ef8734e8a0c22828926d157898ad7ba4e1b
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2020-12-17 17:02:34 +0100

    Extract products from WDC

--- src/data/webdatacommons/extract_product_names.py
+++ src/data/webdatacommons/extract_product_names.py
@@ -12,29 +12,72 @@ from src.data.preprocessing import preprocess
 @click.option('--output_path', help='Path to file containing products')
 def main(file_path, output_path):
     logger = logging.getLogger(__name__)
-    with gzip.open(file_path, 'rt', encoding='utf-8') as f:
+
+    categories = set()
+    breadcrumbs = set()
+    breadcrumblists = set()
+
+
+    with open(file_path, 'rt', encoding='utf-8') as f:
         with open(output_path, 'w', encoding='utf-8') as out_f:
             counter = 0
+            product = {'Title': 'Title', 'Description': 'Description', 'Category': 'Category',
+                       'Breadcrumb': 'Breadcrumb', 'BreadcrumbList': 'BreadcrumbList'}
+            uri = 'initial'
             for i, line in enumerate(f):
                 reader = csv.reader([line], delimiter= ' ', quotechar='"')
                 try:
                     for r in reader:
+                        if r[3] != uri:
+                            uri = r[3]
+                            if len(product['Title']) > 0 and \
+                                    (len(product['Category']) > 0 \
+                                or len(product['Breadcrumb']) > 0 \
+                                or len(product['BreadcrumbList']) > 0):
+
+                                line = '{};{};{};{};{}\n'.format(product['Title'], product['Description'],
+                                                              product['Category'], product['Breadcrumb'],
+                                                              product['BreadcrumbList'])
+                                out_f.write(line)
+                                # Initialize product dict
+                                product = {key: '' for key in product }
+                                counter += 1
+
+                                if counter % 100 == 0:
+                                    logger.info('Written {} product names to disc.'.format(counter))
+
+                                if counter == 100000:
+                                    break
+
                         if r[1] == '<http://schema.org/Product/name>' and '@en' in r[2]:
+                            product['Title'] = preprocess(r[2].split('@')[0].replace('\\n', ''))
 
-                            title = r[2].split('@')[0]
-                            prep_title = preprocess(title)
-                            line = '{} \n'.format(prep_title)
-                            out_f.write(line)
-                            counter += 1
+                        if r[1] == '<http://schema.org/Product/description>':
+                            product['Description'] = preprocess(r[2].split('@')[0].replace('\\n', ''))
 
-                            if counter % 100 == 0:
-                                logger.info('Written {} product names to disc.'.format(counter))
+                        if 'category' in r[1]:
+                            categories.add(r[1])
+                            product['Category'] = preprocess(r[2].split('@')[0].replace('\\n', ''))
 
-                            if counter == 100000:
-                                break
+                        if 'breadcrumb' in r[1]:
+                            breadcrumbs.add(r[1])
+                            product['Breadcrumb'] = preprocess(r[2].split('@')[0].replace('\\n', ''))
+
+                        if 'breadcrumblist' in r[1]:
+                            breadcrumblists.add(r[1])
+                            product['BreadcrumbList'] = preprocess(r[2].split('@')[0].replace('\\n', ''))
 
                 except csv.Error as e:
                     print(e)
+
+    for value in categories:
+        logger.info('Category value: {}'.format(value))
+
+    for value in breadcrumbs:
+        logger.info('Breadcrumbs value: {}'.format(value))
+
+    for value in breadcrumblists:
+        logger.info('Breadcrumblists value: {}'.format(value))
 
     logger.info('Written {} product names to disc.'.format(counter))
 
