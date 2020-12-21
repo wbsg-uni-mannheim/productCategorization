616a86979d20882ad8cf922101cf743e19bcf884
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2020-12-21 09:33:13 +0100

    Update Host extraction for WDC Product Corpus

--- src/data/wdc_ziqi/extract_hosts.py
+++ src/data/wdc_ziqi/extract_hosts.py
@@ -5,12 +5,16 @@ import click
 from src.data.make_dataset import load_line_json
 
 @click.command()
-@click.option('--file_path', help='Path to file containing products')
+@click.option('--file_paths', help='Path to file containing products', multiple=True)
 @click.option('--output_path', help='Path to output file')
-def main(file_path, output_path):
-    df_data = load_line_json(file_path)
-    hosts = df_data['URL'].values
-    hosts = set([extract_host(host) for host in hosts])
+def main(file_paths, output_path):
+    hosts = set()
+
+    for file_path in file_paths:
+        df_data = load_line_json(file_path)
+        new_hosts = df_data['URL'].values
+        for host in new_hosts:
+            hosts.add(extract_host(host))
 
     with open(output_path, 'w') as out_file:
         for host in hosts:
